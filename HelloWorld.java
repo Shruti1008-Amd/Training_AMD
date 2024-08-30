b09ae893e8d51796363c40012a6fc02a02fb7c76
Shruti1008-Amd <gugaleshruti2001@gmail.com>
2024-08-30 12:50:38 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+  static void method(){
+    System.out.println("Hello Java");
+    System.out.println("Hello Chnaged");
+  }
+
+  public static void main(String[] args){
+    System.out.println("Hello java world");
+    method();
+    
+  }
+  
+}
