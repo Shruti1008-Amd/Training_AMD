7860780cdb0113d6116eafdb37dd0dfa3086a2fd
Shruti1008-Amd <gugaleshruti2001@gmail.com>
2024-08-30 13:08:08 +0530

    Added method 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,13 @@ public class HelloWorld{
     System.out.println("Hello Chnaged");
   }
 
+	static void method2(){
+
+System.out.println("Hello Java method 2");
+    System.out.println("Hello Chnaged mwthod 2");
+	
+}
+
   public static void main(String[] args){
     System.out.println("Hello java world");
     method();
